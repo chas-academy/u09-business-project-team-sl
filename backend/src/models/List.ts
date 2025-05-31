8d108d88969bae2f7f99db810dec6b7f5ea97535
Lara <lara.salman01@hotmail.com>
2025-05-31 16:05:31 +0200

    feat: update list model to add games from API

--- backend/src/models/List.ts
+++ backend/src/models/List.ts
@@ -1,15 +1,33 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
 
+interface IGame {
+  rawgId: string;
+  title: string;
+  releaseDate: string;
+  platforms: string[];
+  image: string;
+}
+
 export interface IList extends Document {
   title: string;
   description?: string;
   userId: Types.ObjectId;
+  games: IGame[];
 }
+
+const gameSchema = new Schema<IGame>({
+  rawgId: { type: String, required: true },
+  title: { type: String, required: true },
+  releaseDate: { type: String, required: true },
+  platforms: [{ type: String }],
+  image: { type: String, required: true },
+});
 
 const listSchema = new Schema<IList>({
   title: { type: String, required: true },
   description: { type: String, default: "" },
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  games: { type: [gameSchema], default: [] },
 });
 
 const List = mongoose.model<IList>("List", listSchema);
