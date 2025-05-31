acb33919a2ec0ec4fdc74e602f0a1694bd5e7dd5
Lara <lara.salman01@hotmail.com>
2025-05-31 16:14:52 +0200

    fix: remove double route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,6 @@ app.use(express.json());
 app.use("/api", rawgRoutes); // RAWG API
 app.use("/api/auth", authRoutes);
 app.use("/api/lists", listRoutes);
-app.use("/api/lists", listRoutes);
 
 // Connect to mongoDB
 connectDB();
