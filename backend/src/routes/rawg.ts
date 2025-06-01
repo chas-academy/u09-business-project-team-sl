20dec61e2da831f6e4006fc6d0788d2f8eaf1867
Lara <lara.salman01@hotmail.com>
2025-06-01 21:22:36 +0200

    feat: add route to specific game

--- backend/src/routes/rawg.ts
+++ backend/src/routes/rawg.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import { getGameDetails } from "../controllers/rawg.controller";
 
 dotenv.config();
 
@@ -8,7 +9,7 @@ const router = Router();
 const RAWG_API_KEY = process.env.RAWG_API_KEY;
 const BASE_URL = "https://api.rawg.io/api";
 
-// GET /api/games
+// fetch all hames
 router.get("/games", async (_req, res) => {
   try {
     const url = `${BASE_URL}/games?key=${RAWG_API_KEY}&page_size=10`;
@@ -25,5 +26,8 @@ router.get("/games", async (_req, res) => {
     res.status(500).json({ message: "Failed to fetch games from RAWG API" });
   }
 });
+
+// fetch specific game
+router.get("/games/:id", getGameDetails);
 
 export default router;
