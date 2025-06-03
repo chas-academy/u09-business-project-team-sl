a0d7c3a11eb23606997a7983b55391ddd9b875b3
Lara <lara.salman01@hotmail.com>
2025-06-03 18:09:10 +0200

    feat: add route to remove a game from a specific list

--- backend/src/routes/list.ts
+++ backend/src/routes/list.ts
@@ -5,7 +5,8 @@ import {
   getListById,
   updateList,
   deleteList,
-  addGameToList
+  addGameToList,
+  removeGameFromList
 } from "../controllers/list.controller";
 import verifyToken from "../middleware/verifyToken";
 
@@ -18,5 +19,6 @@ router.get("/:id", verifyToken, getListById);
 router.put("/:id", verifyToken, updateList);
 router.delete("/:id", verifyToken, deleteList);
 router.post("/:id/add-game", verifyToken, addGameToList);
+router.delete("/:id/games/:rawgId", verifyToken, removeGameFromList);
 
 export default router;
