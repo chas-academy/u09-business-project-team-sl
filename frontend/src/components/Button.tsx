ddefefe3125c87e1fc77b035f5a1f97e4c51aad2
Lara <lara.salman01@hotmail.com>
2025-06-04 23:32:46 +0200

    feat: add active state

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -15,24 +15,28 @@ const Button: React.FC<ButtonProps> = ({
   variant = "primary",
   children,
   icon,
-  iconClass = "text-shade-950 group-hover:text-shade-50",
+  iconClass = "text-shade-950 group-hover:text-shade-50 group-active:text-shade-50",
   onClick,
   type = "button",
   className = "",
 }) => {
   const baseStyles =
-    "inline-flex w-fit items-center justify-center text-base font-bold px-4 py-2 rounded-lg gap-1 cursor-pointer shadow"; // Added neutral shadow to preserve inset
+    "inline-flex w-fit items-center justify-center text-base font-bold px-4 py-2 rounded-lg gap-1 cursor-pointer shadow group";
 
   const variantStyles =
     variant === "primary"
-      ? "bg-primary text-shade-950 shadow-[inset_0_0_10px_5px_#2B6BE3] hover:bg-[#2B6BE3] hover:text-shade-50 hover:shadow-[inset_0_0_10px_5px_#124CB7]"
-      : "bg-shade-400 text-shade-950 shadow-[inset_0_0_10px_5px_#4F4F4F] hover:bg-[#C31010] hover:text-shade-50 hover:shadow-[inset_0_0_10px_5px_rgba(0,0,0,0.5)]";
+      ? "bg-primary text-shade-950 shadow-[inset_0_0_10px_5px_#2B6BE3] " +
+        "hover:bg-[#2B6BE3] hover:text-shade-50 hover:shadow-[inset_0_0_10px_5px_#124CB7] " +
+        "active:bg-[#2B6BE3] active:text-shade-50 active:shadow-[inset_0_0_10px_5px_#124CB7]"
+      : "bg-shade-400 text-shade-950 shadow-[inset_0_0_10px_5px_#4F4F4F] " +
+        "hover:bg-[#C31010] hover:text-shade-50 hover:shadow-[inset_0_0_10px_5px_rgba(0,0,0,0.5)] " +
+        "active:bg-[#C31010] active:text-shade-50 active:shadow-[inset_0_0_10px_5px_rgba(0,0,0,0.5)]";
 
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`${baseStyles} ${variantStyles} ${className} group`}
+      className={`${baseStyles} ${variantStyles} ${className}`}
     >
       {children}
       {icon && <Icon icon={icon} className={iconClass} />}
