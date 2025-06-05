10b53ea4d262df4650f0d10ff5e1ba541ffe7fa0
Lara <lara.salman01@hotmail.com>
2025-06-05 16:41:10 +0200

    feat: add navbar to layout

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const Layout = () => {
   return (
     <div className="flex flex-col items-center bg-[url('/background.png')] bg-cover bg-center min-h-screen">
-      {/* Navbar here */}
+      <Navbar isLoggedIn={true} />
 
       <main className="w-full flex-1 p-20 md:p-16 px-4">
         <Outlet />
