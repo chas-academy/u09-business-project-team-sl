5249b92d6ce3a54d6de2829472201731046a4d33
SRsoz <sozyar.rauf@chasacademy.se>
2025-06-06 19:17:56 +0200

    created delete component

---
+++ frontend/src/components/delete.tsx
@@ -0,0 +1,20 @@
+import { Icon } from "@iconify/react";
+
+type deleteProps = {
+  onClick: () => void;
+};
+
+const DeleteButton: React.FC<deleteProps> = ({ onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="w-5 h-5 flex items-center justify-center cursor-pointer rounded transition-colors hover:text-[#C31010] text-shade-50"
+    >
+      <Icon
+        icon="akar-icons:cross"
+      />
+    </button>
+  );
+};
+
+export default DeleteButton;
