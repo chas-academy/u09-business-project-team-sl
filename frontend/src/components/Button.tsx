086e72aa83d628490877185597fe896c9f56e01a
Lara <lara.salman01@hotmail.com>
2025-06-06 18:21:18 +0200

    feat: add disabled mode to button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -10,6 +10,7 @@ type ButtonProps = {
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
   className?: string;
+  disabled?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({
@@ -20,17 +21,25 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
   type = "button",
   className = "",
+  disabled = false,
 }) => {
   // Base style for all variants
-  const baseStyles =
-    "inline-flex w-fit items-center justify-center text-base font-bold px-4 py-2 rounded-lg gap-1 cursor-pointer shadow group";
+  const baseStyles = `
+    inline-flex w-fit items-center justify-center text-base font-bold px-4 py-2 
+    rounded-lg gap-1 shadow group transition
+    ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}
+  `;
 
   // Different styles based on button variant
   const variantStyles =
     variant === "primary"
-      ? "bg-primary text-shade-950 shadow-[inset_0_0_10px_5px_#2B6BE3] " +
-        "hover:bg-[#2B6BE3] hover:text-shade-50 hover:shadow-[inset_0_0_10px_5px_#124CB7] " +
-        "active:bg-[#2B6BE3] active:text-shade-50 active:shadow-[inset_0_0_10px_5px_#124CB7]"
+      ? disabled
+        ? "bg-primary text-shade-950" // no hover or active state if button is disabled
+        : "bg-primary text-shade-950 shadow-[inset_0_0_10px_5px_#2B6BE3] " +
+          "hover:bg-[#2B6BE3] hover:text-shade-50 hover:shadow-[inset_0_0_10px_5px_#124CB7] " +
+          "active:bg-[#2B6BE3] active:text-shade-50 active:shadow-[inset_0_0_10px_5px_#124CB7]"
+      : disabled
+      ? "bg-shade-400 text-shade-950"
       : "bg-shade-400 text-shade-950 shadow-[inset_0_0_10px_5px_#4F4F4F] " +
         "hover:bg-[#C31010] hover:text-shade-50 hover:shadow-[inset_0_0_10px_5px_rgba(0,0,0,0.5)] " +
         "active:bg-[#C31010] active:text-shade-50 active:shadow-[inset_0_0_10px_5px_rgba(0,0,0,0.5)]";
@@ -39,6 +48,7 @@ const Button: React.FC<ButtonProps> = ({
     <button
       type={type}
       onClick={onClick}
+      disabled={disabled}
       className={`${baseStyles} ${variantStyles} ${className}`}
     >
       {children}
