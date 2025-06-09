78d389eecbe5c2905ffd7b01bec568593e4525a4
Lara <lara.salman01@hotmail.com>
2025-06-09 18:47:29 +0200

    feat: add Register route to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
           {/* Add pages here */}
         </Route>
       </Routes>
