dea8a587bed5f94aa47bb8af16b85067eb90a4bf
Lara <lara.salman01@hotmail.com>
2025-06-11 06:49:19 +0200

    feat: add route to lists in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/home";
 import Login from "./pages/Login";
-import CreateList from "./pages/createList";
 import Register from "./pages/Register";
 import GameDetail from "./pages/GameDetail";
+import CreateList from "./pages/createList";
+import Lists from "./pages/Lists";
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
-          <Route path="createList" element={<CreateList />} />
           <Route path="register" element={<Register />} />
           <Route path="games/:id" element={<GameDetail />} />
+          <Route path="createList" element={<CreateList />} />
+          <Route path="lists" element={<Lists />} />
           {/* Add pages here */}
         </Route>
       </Routes>
