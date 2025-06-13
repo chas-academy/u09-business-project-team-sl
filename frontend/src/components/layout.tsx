253a559f131e15e636268ff1060ce9129e3a9be9
Lara <lara.salman01@hotmail.com>
2025-06-14 00:16:45 +0200

    fix: update navbar import in layout.tsx

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import { useAuth } from "../context/AuthContext";
 
 const Layout = () => {
+  const { isLoggedIn } = useAuth();
+
   return (
     <div className="flex flex-col items-center bg-[url('/background.png')] bg-cover bg-center min-h-screen">
-      <Navbar isLoggedIn={false} />
+      <Navbar isLoggedIn={isLoggedIn} />
 
       <main className="w-full flex-1 p-20 md:p-16 px-4">
         <Outlet />
